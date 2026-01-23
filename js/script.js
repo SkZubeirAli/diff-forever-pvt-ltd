7a1a76da08469158fa266357051f4f97ca359826
SkZubeirAli <zuber.nexgeno@gmail.com>
2026-01-23 05:42:54 +0530

    make dummy page for common header and footer

--- js/script.js
+++ js/script.js
@@ -54,7 +54,19 @@ window.addEventListener("scroll", function () {
 });
 
 
+fetch("/partials/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        });
 
+    fetch("/partials/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+
+        
 
 document.addEventListener("DOMContentLoaded", function () {
 
